361f096fd6e8409ec9ede607c8bd16b91ff63a0c
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2020-04-19 21:07:34 -0600

    Bigmanager

--- Proyecto_2/src/proyecto_2/BitManager.java
+++ Proyecto_2/src/proyecto_2/BitManager.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto_2;
 
-/**
- *
- * @author brand
- */
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class BitManager {
-    
+   private ObjectInputStream ois;
+   private ObjectOutputStream oos;
+   
+   public void writeObject(Object object) {
+   try{
+       oos=new ObjectOutputStream(new FileOutputStream("file.bin"));
+   oos.writeObject(object);
+   oos.close();
+   }catch (Exception e){
+       System.out.println(e.getMessage());       
+   }
+   }
+   
+   
+    public Object readObject () {
+        Object object;
+   try{
+       ois=new ObjectInputStream(new FileInputStream("file.bin"));
+       object=ois.readObject();
+       return object;
+   }catch (Exception e){
+       
+       System.out.println(e.getMessage());   
+       return null;
+   }
+   }
+   
 }
