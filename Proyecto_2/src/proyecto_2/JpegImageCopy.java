bbeac9a421d257daffebababd72d7e81b8626647
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2020-04-27 22:32:10 -0600

    apartadobiblitoecafinalizado

---
+++ Proyecto_2/src/proyecto_2/JpegImageCopy.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_2;
+
+/**
+ *
+ * @author brand
+ */
+public class JpegImageCopy extends ImageHandler {
+
+    public JpegImageCopy(String filename) {
+        super(filename);
+    }
+
+    @Override
+    public void readFile() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void generateFiles() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
